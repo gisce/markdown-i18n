bc9261336a0640a4d7fce1de2deb1c90e622dce1
Eduard Carreras <ecarreras@gisce.net>
2017-01-02 14:03:27 +0100

    Refactoring tests

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -29,119 +29,104 @@ class I18nTest(unittest.TestCase):
     """Basic test for i18n jinja
     """
 
+    def setUp(self):
+        self.dir = tempfile.mkdtemp()
+        locale = 'es_ES'
+        self.catalog = catalog.Catalog(locale=locale)
+        os.mkdir(os.path.join(self.dir, locale))
+        lc_messages = os.path.join(self.dir, locale, 'LC_MESSAGES')
+        os.mkdir(lc_messages)
+        self.mo_file = os.path.join(lc_messages, 'messages.mo')
+
+    def tearDown(self):
+        shutil.rmtree(self.dir)
+
+    def write_mo(self):
+        with open(self.mo_file, 'w') as f:
+            mofile.write_mo(f, self.catalog)
+
+    def markdown(self, text, extensions=None, extension_configs=None):
+        if extensions is None:
+            extensions = []
+        extensions.append('markdown_i18n')
+
+        if extension_configs is None:
+            extension_configs = {}
+
+        if 'markdown_i18n' not in extension_configs:
+            extension_configs['markdown_i18n'] = {
+                'i18n_dir': self.dir,
+                'i18n_lang': 'es_ES'
+            }
+
+        return markdown(
+            text,
+            extensions=extensions,
+            extension_configs=extension_configs
+        )
+
     def test_create_pot_file(self):
-        with TempDir() as d:
-            pot_file = os.path.join(d.dir, 'messages.pot')
 
-            text = "this is a simple text"
-            markdown(
-                text,
-                extensions=['markdown_i18n'],
-                extension_configs={'markdown_i18n': {'i18n_dir': d.dir}}
-            )
-            self.assertTrue(os.path.exists(pot_file))
+        pot_file = os.path.join(self.dir, 'messages.pot')
 
-            with open(pot_file, 'r') as f:
-                catalog = pofile.read_po(f)
-                self.assertIn(text, catalog)
-                self.assertEqual(len(catalog), 1)
+        text = "this is a simple text"
+        self.markdown(text)
+        self.assertTrue(os.path.exists(pot_file))
+
+        with open(pot_file, 'r') as f:
+            catalog = pofile.read_po(f)
+            self.assertIn(text, catalog)
+            self.assertEqual(len(catalog), 1)
 
     def test_basic_text(self):
         text = "this is a simple text"
         expected = '<p>esto es un simple test</p>'
-        with TempDir() as d:
 
-            c = catalog.Catalog(locale='es_ES')
-            c.add("this is a simple text", "esto es un simple test")
-            os.mkdir(os.path.join(d.dir, 'es_ES'))
-            lc_messages = os.path.join(d.dir, 'es_ES', 'LC_MESSAGES')
-            os.mkdir(lc_messages)
-            mo_file = os.path.join(lc_messages, 'messages.mo')
-            with open(mo_file, 'w') as f:
-                mofile.write_mo(f, c)
+        self.catalog.add("this is a simple text", "esto es un simple test")
+        self.write_mo()
 
-            result = markdown(
-                text,
-                extensions=['markdown_i18n'],
-                extension_configs={
-                    'markdown_i18n': {'i18n_dir': d.dir, 'i18n_lang': 'es_ES'}
-                }
-            )
+        result = self.markdown(text)
         self.assertEqual(expected, result)
 
     def test_newline_text(self):
         text = "this is a simple\ntext"
         expected = '<p>esto es un simple\ntest</p>'
-        with TempDir() as d:
-            c = catalog.Catalog(locale='es_ES')
-            c.add("this is a simple\ntext", "esto es un simple\ntest")
-            os.mkdir(os.path.join(d.dir, 'es_ES'))
-            lc_messages = os.path.join(d.dir, 'es_ES', 'LC_MESSAGES')
-            os.mkdir(lc_messages)
-            mo_file = os.path.join(lc_messages, 'messages.mo')
-            with open(mo_file, 'w') as f:
-                mofile.write_mo(f, c)
 
-            result = markdown(
-                text,
-                extensions=['markdown_i18n'],
-                extension_configs={
-                    'markdown_i18n': {'i18n_dir': d.dir, 'i18n_lang': 'es_ES'}
-                }
-            )
+        self.catalog.add("this is a simple\ntext", "esto es un simple\ntest")
+        self.write_mo()
+
+        result = self.markdown(text)
         self.assertEqual(expected, result)
 
     def test_quoted_text(self):
         text = 'this is a simple "text"'
         expected = '<p>esto es un simple "test"</p>'
-        with TempDir() as d:
-            c = catalog.Catalog(locale='es_ES')
-            c.add('this is a simple "text"', 'esto es un simple "test"')
-            os.mkdir(os.path.join(d.dir, 'es_ES'))
-            lc_messages = os.path.join(d.dir, 'es_ES', 'LC_MESSAGES')
-            os.mkdir(lc_messages)
-            mo_file = os.path.join(lc_messages, 'messages.mo')
-            with open(mo_file, 'w') as f:
-                mofile.write_mo(f, c)
 
-            result = markdown(
-                text,
-                extensions=['markdown_i18n'],
-                extension_configs={
-                    'markdown_i18n': {'i18n_dir': d.dir, 'i18n_lang': 'es_ES'}
-                }
-            )
+        self.catalog.add('this is a simple "text"', 'esto es un simple "test"')
+        self.write_mo()
+
+        result = self.markdown(text)
         self.assertEqual(expected, result)
 
     def test_multi_paragraph(self):
         text = "paragraph 1\n\nparagraph 2"
         expected = '<p>parrafo 1</p>'
         expected += '<p>parrafo 2</p>'
-        with TempDir() as d:
-            c = catalog.Catalog(locale='es_ES')
-            c.add('paragraph 1', 'parrafo 1')
-            c.add('paragraph 2', 'parrafo 2')
-            os.mkdir(os.path.join(d.dir, 'es_ES'))
-            lc_messages = os.path.join(d.dir, 'es_ES', 'LC_MESSAGES')
-            os.mkdir(lc_messages)
-            mo_file = os.path.join(lc_messages, 'messages.mo')
-            with open(mo_file, 'w') as f:
-                mofile.write_mo(f, c)
 
-            result = markdown(
-                text,
-                extensions=['markdown_i18n'],
-                extension_configs={
-                    'markdown_i18n': {'i18n_dir': d.dir, 'i18n_lang': 'es_ES'}
-                }
-            )
+        self.catalog.add('paragraph 1', 'parrafo 1')
+        self.catalog.add('paragraph 2', 'parrafo 2')
+        self.write_mo()
+
+        result = self.markdown(text)
         self.assertEqual(expected, result)
 
     def test_headers(self):
         for x in range(1, 7):
             text = "{0} This is a h{1}".format('#' * x, x)
             expected = '<h{0} id="this-is-a-h{0}">Esto es un h{0}</h{0}>'.format(x)
+
             with TempDir() as d:
+
                 c = catalog.Catalog(locale='es_ES')
                 c.add('This is a h{0}'.format(x), 'Esto es un h{0}'.format(x))
                 os.mkdir(os.path.join(d.dir, 'es_ES'))
@@ -151,14 +136,17 @@ class I18nTest(unittest.TestCase):
                 with open(mo_file, 'w') as f:
                     mofile.write_mo(f, c)
 
-                result = markdown(
+                result = self.markdown(
                     text,
-                    extensions=['markdown.extensions.toc', 'markdown_i18n'],
+                    extensions=['markdown.extensions.toc'],
                     extension_configs={
-                        'markdown_i18n': {'i18n_dir': d.dir, 'i18n_lang': 'es_ES'}
+                        'markdown_i18n': {
+                            'i18n_dir': d.dir,
+                            'i18n_lang': 'es_ES'
+                        }
                     }
                 )
-            self.assertEqual(expected, result)
+                self.assertEqual(expected, result)
 
     def test_ulists(self):
         text = "* First element.\n * Second element.\n"
@@ -167,24 +155,12 @@ class I18nTest(unittest.TestCase):
 
 <li>Segundo elemento.</li>
 </ul>"""
-        with TempDir() as d:
-            c = catalog.Catalog(locale='es_ES')
-            c.add("First element.", "Primer elemento.")
-            c.add("Second element.", "Segundo elemento.")
-            os.mkdir(os.path.join(d.dir, 'es_ES'))
-            lc_messages = os.path.join(d.dir, 'es_ES', 'LC_MESSAGES')
-            os.mkdir(lc_messages)
-            mo_file = os.path.join(lc_messages, 'messages.mo')
-            with open(mo_file, 'w') as f:
-                mofile.write_mo(f, c)
 
-            result = markdown(
-                text,
-                extensions=['markdown_i18n'],
-                extension_configs={
-                    'markdown_i18n': {'i18n_dir': d.dir, 'i18n_lang': 'es_ES'}
-                }
-            )
+        self.catalog.add("First element.", "Primer elemento.")
+        self.catalog.add("Second element.", "Segundo elemento.")
+        self.write_mo()
+
+        result = self.markdown(text)
         self.assertEqual(clean_xml(expected), clean_xml(result))
 
     def test_nlists(self):
@@ -194,54 +170,32 @@ class I18nTest(unittest.TestCase):
 
 <li>Segundo elemento.</li>
 </ol>"""
-        with TempDir() as d:
-            c = catalog.Catalog(locale='es_ES')
-            c.add("First element.", "Primer elemento.")
-            c.add("Second element.", "Segundo elemento.")
-            os.mkdir(os.path.join(d.dir, 'es_ES'))
-            lc_messages = os.path.join(d.dir, 'es_ES', 'LC_MESSAGES')
-            os.mkdir(lc_messages)
-            mo_file = os.path.join(lc_messages, 'messages.mo')
-            with open(mo_file, 'w') as f:
-                mofile.write_mo(f, c)
 
-            result = markdown(
-                text,
-                extensions=['markdown_i18n'],
-                extension_configs={
-                    'markdown_i18n': {'i18n_dir': d.dir,
-                                      'i18n_lang': 'es_ES'}
-                }
-            )
+        self.catalog.add("First element.", "Primer elemento.")
+        self.catalog.add("Second element.", "Segundo elemento.")
+        self.write_mo()
+
+        result = self.markdown(text)
         self.assertEqual(clean_xml(expected), clean_xml(result))
 
     def test_merge_existing_pot(self):
-        with TempDir() as d:
-            pot_file = os.path.join(d.dir, 'messages.pot')
 
-            text1 = "this is a simple text"
-            markdown(
-                text1,
-                extensions=['markdown_i18n'],
-                extension_configs={'markdown_i18n': {'i18n_dir': d.dir}}
-            )
+        pot_file = os.path.join(self.dir, 'messages.pot')
 
-            self.assertTrue(os.path.exists(pot_file))
+        text1 = "this is a simple text"
+        self.markdown(text1)
 
-            text2 = "another text"
-            markdown(
-                text2,
-                extensions=['markdown_i18n'],
-                extension_configs={'markdown_i18n': {'i18n_dir': d.dir}}
-            )
+        self.assertTrue(os.path.exists(pot_file))
 
-            with open(pot_file, 'r') as f:
-                catalog = pofile.read_po(f)
-                self.assertEqual(len(catalog), 2)
-                self.assertIn(text1, catalog)
-                self.assertIn(text2, catalog)
+        text2 = "another text"
+        self.markdown(text2)
 
-    @unittest.skip('working on it')
+        with open(pot_file, 'r') as f:
+            po_file = pofile.read_po(f)
+            self.assertEqual(len(po_file), 2)
+            self.assertIn(text1, po_file)
+            self.assertIn(text2, po_file)
+
     def test_no_translate_code(self):
         text = ('```bash\n'
                 '$ python --version\n'
@@ -254,15 +208,12 @@ class I18nTest(unittest.TestCase):
                     '$ pip --version\n'
                     'pip 1.5.2\n'
                     '</code></pre>')
-        with TempDir() as d:
-            result = markdown(
-                text,
-                extensions=['markdown_i18n'],
-                extension_configs={
-                    'markdown_i18n': {'i18n_dir': d.dir, 'i18n_lang': 'es_ES'}
-                }
-            )
-        self.assertEqual(expected, result)
+
+        result = self.markdown(
+            text,
+            extensions=['markdown.extensions.fenced_code']
+        )
+        self.assertEqual(clean_xml(expected), clean_xml(result))
 
     def test_tables(self):
         text = """
@@ -284,63 +235,39 @@ Content 1     | Content 2
 </tr>
 </tbody>
 </table>"""
-        with TempDir() as d:
 
-            c = catalog.Catalog(locale='es_ES')
-            c.add("First Header", "Encabezamiento primero")
-            c.add("Second Header", "Encabezamiento segundo")
-            c.add("Content 1", "Contenido 1")
-            c.add("Content 2", "Contenido 2")
-            os.mkdir(os.path.join(d.dir, 'es_ES'))
-            lc_messages = os.path.join(d.dir, 'es_ES', 'LC_MESSAGES')
-            os.mkdir(lc_messages)
-            mo_file = os.path.join(lc_messages, 'messages.mo')
-            with open(mo_file, 'w') as f:
-                mofile.write_mo(f, c)
+        self.catalog.add("First Header", "Encabezamiento primero")
+        self.catalog.add("Second Header", "Encabezamiento segundo")
+        self.catalog.add("Content 1", "Contenido 1")
+        self.catalog.add("Content 2", "Contenido 2")
+        self.write_mo()
 
-            result = markdown(
-                text,
-                extensions=['markdown_i18n', 'markdown.extensions.tables'],
-                extension_configs={
-                    'markdown_i18n': {'i18n_dir': d.dir, 'i18n_lang': 'es_ES'}
-                }
-            )
+        result = self.markdown(
+            text,
+            extensions=['markdown.extensions.tables']
+        )
         self.assertEqual(clean_xml(expected), clean_xml(result))
 
     def test_admonition(self):
 
-        with TempDir() as d:
+        text = (
+            "!!!note\n"
+            "    This is a note."
+        )
 
-            text = (
-                "!!!note\n"
-                "    This is a note."
-            )
+        expected = (
+            '<div class="admonition note">'
+            '    <p class="admonition-title">Note</p>'
+            '    <p>Esto es una nota.</p>'
+            '</div>'
+        )
 
-            expected = (
-                '<div class="admonition note">'
-                '    <p class="admonition-title">Note</p>'
-                '    <p>Esto es una nota.</p>'
-                '</div>'
-            )
+        self.catalog.add("This is a note.", "Esto es una nota.")
+        self.write_mo()
 
-            c = catalog.Catalog(locale='es_ES')
-            c.add("This is a note.", "Esto es una nota.")
-            os.mkdir(os.path.join(d.dir, 'es_ES'))
-            lc_messages = os.path.join(d.dir, 'es_ES', 'LC_MESSAGES')
-            os.mkdir(lc_messages)
-            mo_file = os.path.join(lc_messages, 'messages.mo')
-            with open(mo_file, 'w') as f:
-                mofile.write_mo(f, c)
-
-            result = markdown(
-                text,
-                extensions=['markdown.extensions.admonition', 'markdown_i18n'],
-                extension_configs={
-                    'markdown_i18n': {
-                        'i18n_dir': d.dir,
-                        'i18n_lang': 'es_ES'
-                    }
-                }
-            )
+        result = self.markdown(
+            text,
+            extensions=['markdown.extensions.admonition'],
+        )
 
         self.assertEqual(clean_xml(expected), clean_xml(result))

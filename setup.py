19ae19ed2923e8961a32bff5745eff3ff1d7bbcf
Eduard Carreras <ecarreras@gisce.net>
2017-01-12 11:07:37 +0100

    Bump to v2.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='markdown-i18n',
-    version='2.1.0',
+    version='2.1.1',
     packages=find_packages(),
     url='https://github.com/gisce/markdown-i18n',
     license='MIT',
